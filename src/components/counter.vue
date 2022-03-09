eb786c93d64e0310d8b570776da7eb2a5eb45dc2
estebanvasquex <estebanvasquex@gmail.com>
2022-03-09 14:59:45 -0500

    Se crean botones de incremento y decremento de para el compente counter

--- src/components/counter.vue
+++ src/components/counter.vue
@@ -3,10 +3,11 @@
   <h1>Contador</h1>
   <h2>{{counter}} <sup>2</sup> = {{squareCounter}} propiedad computed</h2>
   <h2>{{counter}} <sup>2</sup> = {{squareCounter}} propiedad computed</h2>
-  <h2>{{counter}} <sup>2</sup> = {{squareCounter}} propiedad computed</h2>
-  <h2>{{counter}} <sup>2</sup> = {{squareCounter}} propiedad computed</h2>
-  <h2>{{esteban()}}</h2> 
-  <h2>{{esteban()}}</h2> 
+  <div>
+      <button @click="increase">+1</button>
+      <button @click="decrease">-1</button>
+  </div>
+
 
   <span> </span>
  
@@ -24,7 +25,16 @@ export default {
         esteban(){
             console.log('esteban')
             return 'Propieda enviada desde methods'
-    }
+        },
+        increase(){
+            this.counter =  this.counter + 1
+        },
+        decrease(){
+            this.counter = this.counter - 1
+        }
+        
+
+
     },
 
     computed: {
@@ -38,5 +48,20 @@ export default {
 </script>
 
 <style>
+button{
+    background-color: #64B687;
+    border-radius: 5px;
+    border: 1px solid white;
+    color: white;
+    cursor: pointer;
+    margin: 0 5px;
+    margin: 5px;
+    transition: 0.3s ease-in-out;
+}
 
+button:hover{
+    background-color: #5aa67b;
+    transition: 0.3s ease-in-out;
+
+}
 </style>
