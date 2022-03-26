cc86d41c8158890f46f927e36173b1be7c82e4e5
estebanvasquex <estebanvasquex@gmail.com>
2022-03-25 19:21:36 -0500

    Se finaliza el comopente que se conecta a la appi yesno funciona correctamente

--- src/components/indecision.vue
+++ src/components/indecision.vue
@@ -1,22 +1,60 @@
 <template>
-<img src="https:/via.placeholder.com/250" alt="">
-<div class="bg-dark"> </div>
-<div class="indecision-container">
-        <input  type="text" placeholder="Hazme una pregunta" name="" id="">
-        <p>Recuerda terminar con el signo de interrogación (?)</p>
-</div>
-<div>
-    <h2>Seré millonario?</h2>
-    <h1>Si, no, .... pensando</h1>
-</div>
+    <img v-if="img" :src="img" alt=""> <!-- El v-if se encarga de revisar si la img existe, como inicia con null entonces no la muestra en el dom, :src="img" trae el link qu entrega la appi de la imagen -->
+    <div class="bg-dark"> </div>
+    <div class="indecision-container">
 
- 
+        <input v-model="question" type="text" placeholder="Hazme una pregunta" name="" id="">
+        <p>Recuerda terminar con el signo de interrogación (?)</p>
+
+        <div v-if="isValidQuestion"> <!-- en este caso muestra el div si isValidQuiestion es verdadera -->
+            <h2>{{question}}</h2>
+            <h1>{{answer}}</h1>
+        </div>
+
+    </div>
 </template>
+
 
 <script>
 export default {
+    data(){
+        return{
+            question:"casa",
+            answer: null,
+            img: null,
+            isValidQuestion: false
+        }
+    },
+    methods: {
+        async getAnswer() {
+            this.answer = "pensando..."
+            const {answer, image} = await fetch("https://yesno.wtf/api").then(r=> r.json()) /* se realiza desestructuración del objeto que entrega la api */
+            /* console.log(answer, image); */
+            this.answer = answer === 'yes'? 'Si!' : 'No!' /* cambia la respuesta para que se vea en español */
+            this.img = image
+        }
 
-}
+
+    },
+
+    watch: {
+        question(value, oldvalue){
+
+            this.isValidQuestion = false; /* Esta propiedad solo se hace verdadera al identificar el "?" y esto lo hace en la instrucción siguiente antes de esto continua en false esto se hace para ocultar el texto donde se encuentra question y answers */
+
+
+            if(!value.includes("?")) return
+
+            this.isValidQuestion = true;
+
+            this.getAnswer()
+                
+            } 
+
+        }
+    }
+    
+
 </script>
 
 <style>
